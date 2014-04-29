1379e476b74460726d72460cd274c4d28204cd8a
Eric Glassman <eglassman@scitent.com>
2014-04-29 17:44:13 -0400

    adds turn gem and updates test helper to choose test suite output formatting options

--- Gemfile
+++ Gemfile
@@ -1,4 +1,7 @@
+source 'https://rubygems.org'
+
 # using git repo as gem source => http://bundler.io/v1.2/git.html 
 
 gem 'rake'
 gem 'minitest'
+gem 'turn'
--- test/lib/test_helper.rb
+++ test/lib/test_helper.rb
@@ -2,4 +2,24 @@ require 'bundler/setup'     # ensures you're loading Gemfile defined gems (http:
 
 gem     'minitest'          # ensures you"re using the gem, and not the built-in MT
 require 'minitest/autorun'  # the easy and explicit way to run all your tests
+require 'turn'
 
+# Configure testsuite output
+# http://blog.rawonrails.com/2012/01/better-minitest-output-with-turn-gem.html
+
+Turn.config do |c|
+ # use one of output formats:
+ # :outline  - turn's original case/test outline mode [default]
+ # :progress - indicates progress with progress bar
+ # :dotted   - test/unit's traditional dot-progress mode
+ # :pretty   - new pretty reporter
+ # :marshal  - dump output as YAML (normal run mode only)
+ # :cue      - interactive testing
+ c.format  = :outline
+
+ # turn on invoke/execute tracing, enable full backtrace
+ c.trace   = true
+
+ # use humanized test names (works only with :outline format)
+ c.natural = true
+end
